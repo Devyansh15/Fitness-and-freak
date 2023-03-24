33508d20b9394aae5a5075ad6bdb01d63bbc3b7b
Devyansh15 <rawatdevyansh@gmail.com>
2023-03-24 23:34:07 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fitness-and-Freak-demo-website
+This is my first website and is created as a practice project
+
+Languages that I used here are : 
+1) HTML
+2) CSS
+3) Javascript
+4) NodeJs
+5) PUG
+6) Mongodb
+
+And that's all...
+
+Thanks 
---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 80;
+const fs = require("fs");
+const { TIMEOUT } = require("dns");
+
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/Fitness-and-freak', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
+
+
+// const ContactSchema = new mongoose.Schema({
+//   name: String,
+//   email : String,
+//   Feedback   : String
+// });
+
+
+// const Contact = mongoose.model('Contact', ContactSchema);
+
+
+
+
+
+// For serving static files
+// Here in " express.static('static') " 'static' inside the paranthesis is a directory name and the other one is the one which appears in the url bar
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+// set the template engine as pug
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// pug endpoint
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/",  (req, res) => {
+  const form = req.body;
+
+
+  fs.writeFileSync(
+    `./Feedback/${req.body.name}.txt`,
+    `Name : ${form.name}\nemail id : ${form.email} \nFeedback : ${form.Feedback}`
+  );
+  setTimeout(myFunc, 3000, "myFunc");
+  function myFunc() {
+    res.render("");
+  }
+
+});
+app.get("/paypage",(req,res)=> {
+  res.render("paypage");
+})
+
+
+app.listen(port, () => {
+  console.log(`Website running on port no. ${port}`);
+});
